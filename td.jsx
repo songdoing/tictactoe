4f09d9af807fc654f42d0ff66e65b2e3a8a988d3
wonjinsong <msmk22239@gmail.com>
2021-02-16 17:37:27 -0500

    optimization1

--- td.jsx
+++ td.jsx
@@ -1,8 +1,17 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import {CLICK_CELL} from './ticTacToe';
 
 const Td = ({rowIndex, cellIndex, dispatch, cellData }) => {
     console.log('td rendered');
+
+    //뭐때문에 td rendered가 계속 되는 지 알려면, useEffect, useRef를 사용
+    const ref = useRef([]);
+    useEffect(()=> {
+        console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]);
+        //false가 나오는게 있으면 걔땨문에 계속 render되는 거임(T, T, T, F)
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+        //각종 프롭스들을 두번째 인자에 넣어두기
+    }, [rowIndex, cellIndex, dispatch, cellData]);
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
         //이미 클릭한 곳은 더이상 클릭 못하게시리
@@ -13,7 +22,8 @@ const Td = ({rowIndex, cellIndex, dispatch, cellData }) => {
         //dispatch({type : CHANGE_TURN});
         //승리 검사하는 와중에 turn이 체인지되면서 오류발생, 승리검사 후에
     }, [cellData]);
-
+    //쉽게, props로 넣어두는 데이터는 useCallback으로 감싸주고
+    //useCallback안에서 바뀔 데이터(cellData)를 맨끝 인자에다가 넣기
     return(
         <td onClick={onClickTd}>{cellData}</td>
     )
--- tr.jsx
+++ tr.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef, useEffect} from 'react';
 import Td from './td';
 
 const Tr = ({ rowData, rowIndex, dispatch }) => {
     console.log('tr rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowData === ref.current[0], dispatch === ref.current[1], rowIndex === ref.current[2]);
+        ref.current = [rowData, dispatch, rowIndex];
+    }, [rowData, dispatch, rowIndex]);
     return(
         <tr>
             {Array(rowData.length).fill().map((td , i) => (
